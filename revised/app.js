42ab6ae2782f8bd0dd5b4f3cd5dbeab16bf9b0af
tikaramac19 <tikaramac19@gmail.com>
2022-06-08 21:16:43 +0545

    uninstall nodemon coz it wont work

--- revised/app.js
+++ revised/app.js
@@ -6,3 +6,20 @@
 // require = function to use modules (Common Js)
 // module = infor about current module (file)
 // process = info about env where the program is being executed
+
+  console.log(__dirname); // it will give the directory name 
+
+    // setInterval(() => {
+    //     console.log("Hello There");
+    // }, 1000);
+
+    setTimeout(() => {
+        console.log("I am timeout build in method.")
+    }, 5000);
+
+  console.log(__filename); // it will give the filename
+
+// tips: we dont need to write everytime node app.js , we can simply write  node app it will work
+
+// Modules
+
