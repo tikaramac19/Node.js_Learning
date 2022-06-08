d43b7f5b8c00c49765a33cdce97a23b0a68c2bbe
tikaramac19 <tikaramac19@gmail.com>
2022-06-08 21:54:35 +0545

    function balen

---
+++ revised/3-balenfunction.js
@@ -0,0 +1,8 @@
+const balenInfo = (fname, lname, bage, position) => {
+  console.log(
+    `${fname} ${lname} is a Rapper and ${position} of Kathmandu metropolitian city. And he is ${bage}`
+  );
+};
+
+
+module.exports = balenInfo;
