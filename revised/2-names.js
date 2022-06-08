1e4f59ccbdddb995921ba62d756cb9ca73a57024
tikaramac19 <tikaramac19@gmail.com>
2022-06-08 21:54:17 +0545

    adding info abt balen

---
+++ revised/2-names.js
@@ -0,0 +1,12 @@
+// local
+const secret = 'SUPER SECRET';
+// for balendra shah
+// share
+const fname = "Balendra";
+const lname = "Shah";
+const bage = 32;
+const position = "Mayor";
+
+// console.log(module);
+
+module.exports = {fname,lname,bage, position}; // export these variable from this module
