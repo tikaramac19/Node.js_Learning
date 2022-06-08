55339b230878eeaec59e5be72579548164117499
tikaramac19 <tikaramac19@gmail.com>
2022-06-08 20:58:48 +0545

    revised

---
+++ revised/1-intro.js
@@ -0,0 +1,12 @@
+// first node app
+
+const number = 12;
+
+    if(number > 10){
+        console.log("big number")
+    }
+    else{
+        console.log("Small Number");
+    }
+
+console.log(`Hey its my first node app.`);
---
+++ revised/app.js
@@ -0,0 +1,8 @@
+// GLOBALS - NO WINDOW
+// in node there is no window coz there is no browser 
+
+// __dirname = path to current directory
+// __filename = file name
+// require = function to use modules (Common Js)
+// module = infor about current module (file)
+// process = info about env where the program is being executed
