10d8a184438956d20960c7b4ae23c6d3628895f3
tikaramac19 <tikaramac19@gmail.com>
2022-06-09 08:56:02 +0545

    fs module

--- restfulApi/userapi/index.js
+++ restfulApi/userapi/index.js
@@ -1,5 +1,5 @@
 const http = require('http');
-
+const fs = require('fs');
 // console.log(http)
 const PORT = 8080;
 
@@ -24,9 +24,21 @@ http.createServer(function(req, res){
             res.end("Welcome to the about Page")
         }
         else if(req.url == '/userapi'){
+
+            fs.readFile(`${__dirname}/userapi.json`, "utf-8", (err, data)=>{
+                    // console.log(data);
+
+                    if(!err){
+                        res.end(data);
+                    }else{
+                        throw err;
+                    }
+
+            });
             res.writeHead(200, {'Content-type' : 'text/html'});
+
             res.end("Welcome to the UserApi Page")
-            
+
         }
         else{
             res.writeHead(401, {'Content-type' : 'text/html'});
