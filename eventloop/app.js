f4e1eb6aa65cd8a7fa7592e66d174d7b4c891710
tikaramac19 <tikaramac19@gmail.com>
2022-06-09 17:02:54 +0545

    emmiting name events

--- eventloop/app.js
+++ eventloop/app.js
@@ -4,20 +4,25 @@
 
 //Example 1 - Registering for the event to be fired only one time using once.
  
+const EventEmitter = require('events');
+// we can create our own event , we can fire that event and listen that event
+const event = new EventEmitter();  // creating object
+
+// define this callback function before we emit it otherwise it wont work properly
+event.on("sayMyName", ()=>{
+    console.log("Your name is Tikaram Ac");
+});
+
+
+event.emit('sayMyName'); // creating new event and emit it  i.e emitting name events
+
+// concept : emitter objects emit named events that cause previously registered listeners to be called. So , an emitter 
+// object basically has two main features:
+// Emitting name eents.
+// Registering and unregistering listener functions.
 
 // Example 2 - Create an eent emitter instance and register a couple of callbacks
 
 
 // Example 3 - Registering for the event with callback parameters.
 
-const EventEmitter = require('events');
-// we can create our own event , we can fire that event and listen that event
-const event = new EventEmitter();  // creating object
-
-event.on("sayMyName", ()=>{
-    console.log("Your name is Tikaram Ac");
-})
-
-
-
-event.emit('sayMyName'); // creating new event and emit it 
