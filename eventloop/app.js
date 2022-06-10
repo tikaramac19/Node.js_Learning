c3b8b08d137e5d0f2f8699e784a97880d5ba43b0
tikaramac19 <tikaramac19@gmail.com>
2022-06-10 20:21:36 +0545

    multiple callback

--- eventloop/app.js
+++ eventloop/app.js
@@ -14,15 +14,38 @@ event.on("sayMyName", ()=>{
 });
 
 
-event.emit('sayMyName'); // creating new event and emit it  i.e emitting name events
+ // creating new event and emit it  i.e emitting name events
 
 // concept : emitter objects emit named events that cause previously registered listeners to be called. So , an emitter 
 // object basically has two main features:
 // Emitting name eents.
 // Registering and unregistering listener functions.
 
-// Example 2 - Create an eent emitter instance and register a couple of callbacks
+// Example 2 - Create an event emitter instance and register a couple of callbacks
+// in node js we can call multiple callbackfunctions using same event
+event.on("sayMyName", ()=>{
+    console.log("Your name is Prakash");
+});
+event.on("sayMyName", ()=>{
+    console.log("Your lastname is Acharya");
+});
+event.on("sayMyName", ()=>{
+    console.log("You live in Birendranagar Surkhet");
+});
 
+event.emit('sayMyName');
+
+// after calling multiple callback function of an event we can do as many task only clicing once. so it is very useful 
 
 // Example 3 - Registering for the event with callback parameters.
 
+const EventEmit = require('events');
+
+const eventttt = new EventEmit();
+
+
+eventttt.on("click", ()=>{
+    console.log("Click me");
+})
+
+eventttt.emit("click");
