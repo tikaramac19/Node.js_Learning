12db186ca2b0f5395dcfbd685de2c6c9aa6ddbb8
tikaramac19 <tikaramac19@gmail.com>
2022-06-18 17:43:44 +0545

    counter logic

--- counter/counterLogic.js
+++ counter/counterLogic.js
@@ -1,1 +1,16 @@
 // A simple counter logic in js
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    function counter(id, start, end, duration){
+        let obj = document.getElementById(id), current = start, range = end - start , increment = end > start ? 1 : -1 ,
+        step = Math.abs(Math.floor(duration/range)),
+        timer = setInterval(()=>{
+            current += increment;
+            obj.textContent = current;
+            if(current == end){
+                clearInterval(timer)
+            }
+        }, step);
+
+    }
+});
