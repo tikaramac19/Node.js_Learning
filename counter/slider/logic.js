278b5df45b7b9184c1590c34b0571a9c5ed8e047
tikaramac19 <tikaramac19@gmail.com>
2022-06-20 15:54:23 +0545

    onload logic

--- counter/slider/logic.js
+++ counter/slider/logic.js
@@ -2,7 +2,7 @@
 
 let i = 0; // start point
 let images = [];
-let time = 3000;
+let time = 4000;
 
 // list of the images
 // i.e array values 
@@ -11,3 +11,22 @@ images[0] = './images/profile.jpg';
 images[1] = './images/pic.jpg';
 images[2] = "./images/tikuprofile.jpg";
 
+// function for change the images
+
+function changeImg(){
+    document.slide.src = images[i];
+    
+    if(i < images.length - 1){
+        i++;
+    }
+    else{
+        i = 0;
+    }
+
+    setTimeout("changeImg()", time);
+
+}
+// we want the images change initially when the window reload
+// so 
+
+window.onload = changeImg;
