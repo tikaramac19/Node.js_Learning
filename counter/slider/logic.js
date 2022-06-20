ea8098f661fefcd576b36ec05d27185fbc9413a3
tikaramac19 <tikaramac19@gmail.com>
2022-06-20 15:44:38 +0545

    images array

---
+++ counter/slider/logic.js
@@ -0,0 +1,13 @@
+// simple slideshow logic
+
+let i = 0; // start point
+let images = [];
+let time = 3000;
+
+// list of the images
+// i.e array values 
+
+images[0] = './images/profile.jpg';
+images[1] = './images/pic.jpg';
+images[2] = "./images/tikuprofile.jpg";
+
