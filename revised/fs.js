f7aac3802a5560020e35506a7ea49052455f073b
tikaramac19 <tikaramac19@gmail.com>
2022-06-23 18:27:30 +0545

    readFile in fs module

--- revised/fs.js
+++ revised/fs.js
@@ -2,7 +2,7 @@
 
 // To include the file system module, use the require() method.
 
-let fs = require('fs');
+// let fs = require('fs');
 
 // console.log(fs);
 
@@ -14,3 +14,22 @@ let fs = require('fs');
 // Delete files 
 // Rename file
 
+// Read Files
+// : The fs.readFile() method is used to read files on our computer 
+
+// Assume we have the  some other files located in the same folder as fs.js
+
+let http = require('http');
+let fs = require('fs');
+
+http.createServer(function(req, res){
+
+    fs.readFile('2-names.js', (err, data)=>{
+        // res.writeHead(200, {'Content-Type': 'text/html'})
+        if(err) throw err;
+
+        return res.end(data);
+    })
+
+
+}).listen(8080);
