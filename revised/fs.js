784667109fbd01b6cbad369666252c825fd036d5
tikaramac19 <tikaramac19@gmail.com>
2022-06-23 18:13:04 +0545

    about file module

---
+++ revised/fs.js
@@ -0,0 +1,16 @@
+// The Node.js file system module allows you to work with the file system on our computer.
+
+// To include the file system module, use the require() method.
+
+let fs = require('fs');
+
+// console.log(fs);
+
+// common use of the file system module.
+
+// . Read files 
+// . create files
+// update files
+// Delete files 
+// Rename file
+
