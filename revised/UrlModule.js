22cc847a3a03b45fd46e356259a13696010e0948
tikaramac19 <tikaramac19@gmail.com>
2022-06-26 19:45:03 +0545

    include URL module

--- revised/UrlModule.js
+++ revised/UrlModule.js
@@ -1,1 +1,8 @@
 // This is the Built-in URL Module
+
+// The URL Module splits up a web address into readable parts.
+// To include the URL module, use the require() method.
+
+let url = require('url');
+
+// Parse an address with the url.parse() method, and it will return a URL object with each part of the address as properties.
