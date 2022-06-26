4dd7e722c006fb1df864499bbe95be8620db54d1
tikaramac19 <tikaramac19@gmail.com>
2022-06-26 20:47:15 +0545

    file server

--- revised/UrlModule.js
+++ revised/UrlModule.js
@@ -35,3 +35,42 @@ console.log(z.host); // it returns www.w3schools.com
 console.log(z.pathname); // it returns /nodejs/nodejs_url.asp
 
 console.log(z.search); // it returns null coz we didnt search anything 
+
+// Node.js file server
+
+// Now we know how to parse the query string, and in the previous chapter we learned how to make Node.js behave as a file server.
+// Let us combine the two, and serve the file requested by the client.
+
+// Create two html files and save them in the same folder as your UrlModule.js files.
+
+// create a file that opens the requested file and returns the content to the client. If anyting goes 
+// wrong , throw a 404 error.
+
+let http = require('http');
+
+let fs = require('fs');
+
+http.createServer(function(req, res){
+
+    let p1 = url.parse(req.url, true);
+
+    let filename = "." + p1.pathname;
+
+    console.log(p1.pathname);
+
+    fs.readFile(filename, function(err, data){
+        if(err){
+            res.writeHead(404, {'Content-Type': 'text/html'});
+
+            return res.end("404 Not found");
+        }
+
+        res.writeHead(200, {'Content-Type': 'text/html'});
+
+        res.write(data);
+
+        return res.end();
+    })
+
+
+}).listen(8080);
