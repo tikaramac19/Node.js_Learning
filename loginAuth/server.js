d75a751b9f2283e7fadb349b193908a0e275becc
tikaramac19 <tikaramac19@gmail.com>
2022-06-27 20:39:35 +0545

    post register complited

--- loginAuth/server.js
+++ loginAuth/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const bcrypt = require('bcrypt'); // used to encrypt the original password
+
 app.set("view-engine", "ejs");
 
+app.use(express.urlencoded({extended:false}));
+
+const users = [];
 
 app.get("/", (req, res)=>{
     res.render("index.ejs");
@@ -25,6 +30,25 @@ app.get("/register", (req,res)=>{
     res.render("register.ejs")
 })
 
+app.post('/register', async(req,res)=>{
+    
+    try{
+        const hashedPassword = await bcrypt.hash(req.body.password,10);
 
+        users.push({
+            id: Date.now().toString(),
+            name : req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        })
+
+        res.redirect('/login');
+
+    }catch{
+        res.redirect('register');
+    }
+// console.log(users);
+
+})
 
 app.listen(port);
