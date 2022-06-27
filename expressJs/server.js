89d1f509bff7bebd699a3e99ed44ee618d534b63
tikaramac19 <tikaramac19@gmail.com>
2022-06-27 17:00:49 +0545

    routes

--- expressJs/server.js
+++ expressJs/server.js
@@ -1,17 +1,37 @@
 const express = require("express");
-
+const { reset } = require("nodemon");
 const app = express();
-
 const port = 8080;
 
-app.get('/' , (req, res)=>{
-    console.log("worked");
+app.set('view engine', "ejs") // rendering html
 
-    res.send("Hello there !")
+app.get('/' , (req, res)=>{
+
+    // console.log("worked");
+    // res.status(200)
+    // res.send("Hello there !")
+    // res.download("server.js")
+
+    // res.json({
+    //     fname :"Tikaram",
+    //     lname : "Acharya"
+    // })
+
+    res.render('index', {text :"World"})  // used to rendering html on the server ie. response
+
 })
 
 
+app.get('/users', (req,res)=>{
+       res.send("Users List"); 
+})
+
+
+app.get("/users/new" , (req, res)=>{
+    res.send("users forms list")
+})
+
 
 app.listen(port, ()=>{
-    console.log(`server is starting at Port http://localhost:${port}`);
+    // console.log(`server is starting at Port http://localhost:${port}`);
 })
