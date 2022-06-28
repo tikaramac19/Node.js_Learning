d26cf7b96b7155662e8710ec2c2b4736cb89a132
tikaramac19 <tikaramac19@gmail.com>
2022-06-28 21:34:00 +0545

    postjs updated

--- expressJs/routes/posts.js
+++ expressJs/routes/posts.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const { route } = require('./users');
 const router = express.Router();
 
 router.get('/', (req, res)=>{
     res.send("This is posts page");
 })
 
+router.post("/", (req,res)=>{
+    res.send("Created Post");
+})
+
+router.get("/:id", (req,res)=>{
+    res.send(`Get the post with ID : ${req.params.id}`)
+})
 module.exports = router;
