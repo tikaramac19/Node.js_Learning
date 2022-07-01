281f20ab82adbd698f6458125c9f26c6d229db72
tikaramac19 <tikaramac19@gmail.com>
2022-07-01 20:42:21 +0545

    file created

---
+++ revised/uploadFile.js
@@ -0,0 +1,16 @@
+// Node JS Upload file 
+
+// The Formidable Module
+
+// There  is a very good module for working with file uploads , called "Formidable".
+
+// THe Formidable module can be downloaded and installed using npm.
+
+// npm install formidable
+
+// After you have downloaded the module, you can include the module in any application you want.
+
+let formidable = require('formidable');
+
+
+// Upload files
