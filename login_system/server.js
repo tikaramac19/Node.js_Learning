bbc023a1c7496d84613039f56ec4d6bc4dd6be42
tikaramac19 <tikaramac19@gmail.com>
2022-07-10 09:27:47 +0545

    bodyparser responsible for incoming http request

--- login_system/server.js
+++ login_system/server.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const path = require('path')
 const app = express();
+const bodyparser = require('body-parser');
+const session = require('express-session');
 
 const port = process.env.PORT || 3000;
+
+app.use(bodyparser.json()); // responsible for passing the incoming http request before we use it
+app.use(bodyparser.urlencoded({extended: true}));
+
+app.use(session({
+    secret: 'secret',
+    resave:false,
+    saveUninitialized: true
+}))
 
 app.set('view engine', 'ejs')
 
 // load static assets
 app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use('/assets', express.static(path.join(__dirname, 'public/assets/')))
+
 // home route
 app.get('/', (req, res) =>{
     res.render('base', {title: "Login system"})
