d2d1a0ceb70ba2078afe5b5b05870abf182ab2af
tikaramac19 <tikaramac19@gmail.com>
2022-07-10 07:45:18 +0545

    load static file

--- login_system/server.js
+++ login_system/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const path = require('path')
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs')
 
+// load static assets
+app.use('/static', express.static(path.join(__dirname, 'public')))
 // home route
 app.get('/', (req, res) =>{
     res.render('base', {title: "Login system"})
