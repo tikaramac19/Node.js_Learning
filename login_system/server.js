031567e473aa25d35ee168fd921b17b7b5e8306c
tikaramac19 <tikaramac19@gmail.com>
2022-07-11 22:50:31 +0545

    encrypt using uuid

--- login_system/server.js
+++ login_system/server.js
@@ -3,23 +3,31 @@ const path = require('path')
 const app = express();
 const bodyparser = require('body-parser');
 const session = require('express-session');
+const {v4:uuidv4} = require('uuid');
+
+const router = require('./router')
 
 const port = process.env.PORT || 3000;
 
 app.use(bodyparser.json()); // responsible for passing the incoming http request before we use it
 app.use(bodyparser.urlencoded({extended: true}));
 
-app.use(session({
-    secret: 'secret',
-    resave:false,
-    saveUninitialized: true
-}))
+
 
 app.set('view engine', 'ejs')
 
 // load static assets
 app.use('/static', express.static(path.join(__dirname, 'public')))
 app.use('/assets', express.static(path.join(__dirname, 'public/assets/')))
+
+app.use(session({
+    secret: uuidv4(),
+    resave:false,
+    saveUninitialized: true
+}))
+
+
+app.use('/route', router);
 
 // home route
 app.get('/', (req, res) =>{
