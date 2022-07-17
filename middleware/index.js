06209d1cd1be88b3ece31de9c6b9536acb4d5065
tikaramac19 <tikaramac19@gmail.com>
2022-07-17 15:16:36 +0545

    explained

--- middleware/index.js
+++ middleware/index.js
@@ -4,7 +4,8 @@ const port = 8080;
 const express = require('express');
 const app = express();
 
-
+// app.use() allows us to add a new middleware function, it accepts an array of 
+// so called request handlers here and it has some other use cases too.
 app.use((req, res, next)=>{
     console.log('in an middleware');
 
@@ -15,9 +16,24 @@ app.use((req,res, next) =>{
     console.log('in an another middleware');
 })
 
+// the function we pass to app.use() will be executed for every incomming 
+// request and this function will receive three arguments.i.e req, res, next()
 
+// Next is actually a function, a function that will be passed to this function 
+// by expressJS 
+
+// next() function weare receiving here has to be executed to allow the 
+// the request to travel on to the next middleware.
 
 
 const server = http.createServer(app);
 
 server.listen(port, ()=> console.log(`server is running at http://localhost:8080`));
+
+// Express js is all about middleware
+// so instead of just having one request handler, you will actually have a possibility of 
+// ... hooking in multiple functions which request will go through until you 
+// send a response
+
+// This allows you to split your code into multiple blocks or pieces instead of 
+// having one huge function that does everything .
