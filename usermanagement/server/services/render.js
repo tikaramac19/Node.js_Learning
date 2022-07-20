64abf7ef743e03d588abe39319b37caa7820064f
tikaramac19 <tikaramac19@gmail.com>
2022-07-20 16:13:11 +0545

    only render the files according to the routes

---
+++ usermanagement/server/services/render.js
@@ -0,0 +1,13 @@
+
+exports.homeRoutes = (req,res)=>{
+    res.render('index'); // rendering index file of the views 
+}
+
+
+exports.add_user = (req, res)=>{
+    res.render('add_user');
+}
+
+exports.update_user = (req, res)=>{
+    res.render('update_user');
+}
