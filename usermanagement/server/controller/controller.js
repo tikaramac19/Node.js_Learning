b43cf00fe2390e38cf3c4f853814bbac458b21eb
tikaramac19 <tikaramac19@gmail.com>
2022-07-20 18:28:27 +0545

    controller for crud operation

---
+++ usermanagement/server/controller/controller.js
@@ -0,0 +1,21 @@
+const Userdb = require('../model/model');
+
+// create and save new user
+exports.create = (req,res)=>{
+
+}
+
+// retrive  and return all users / retrive and return a single user
+exports.find = (req,res)=>{
+    
+}
+
+// update a  new identified user by user id
+exports.update = (req,res)=>{
+
+}
+
+// Delete a user with specified user id in the request
+exports.delete = (req,res)=>{
+    
+}
