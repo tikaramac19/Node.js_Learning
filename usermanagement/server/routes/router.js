6220b4addefbf219963a1c1635fea2b7ffbe87d0
tikaramac19 <tikaramac19@gmail.com>
2022-07-20 18:28:12 +0545

    creating API

--- usermanagement/server/routes/router.js
+++ usermanagement/server/routes/router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const router = express.Router()
 const services = require('../services/render')
-
+const controller = require('../controller/controller')
 // @description Root Router
 // @Method GET/
 
@@ -15,5 +15,15 @@ router.get('/add_user', services.add_user);
 // @description update_user Router
 // @Method GET/update_user
 router.get('/update_user',services.update_user);
+
+// API
+router.post('/api/users', controller.create)
+
+router.get('/api/users', controller.find)
+
+router.put('/api/users/:id', controller.update)
+
+router.delete('/api/users/:id', controller.delete)
+
 
 module.exports = router;
