da2fd4afdc2bcec14937a8ae7c9df30fef4d3b77
tikaramac19 <tikaramac19@gmail.com>
2022-07-20 20:43:01 +0545

    updating the user using id params

--- usermanagement/server/controller/controller.js
+++ usermanagement/server/controller/controller.js
@@ -46,6 +46,25 @@ exports.find = (req,res)=>{
 
 // update a  new identified user by user id
 exports.update = (req,res)=>{
+    if(!req.body){
+        return res
+            .status(400)
+            .send({message: "Data to update can not be empty"})
+    }
+
+    const id = req.params.id;
+
+    Userdb.findByIdAndUpdate(id, req.body, {useFindAndModify: false})
+        .then(data=>{
+            if(!data){
+                res.status(404).send({message:`Cannot update user with ${id}. Maybe user not found !`})
+            }else{
+                res.send(data)
+            }
+        })
+        .catch(err=>{
+            res.status(500).send({message: "Error Update user information"})
+        })
 
 }
 
