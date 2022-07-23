390c01412d7b9dbe6d7875678c2f8ce8cc425f6f
tikaramac19 <tikaramac19@gmail.com>
2022-07-23 17:42:25 +0545

    logic for update the data

--- usermanagement/assets/js/logic.js
+++ usermanagement/assets/js/logic.js
@@ -1,3 +1,25 @@
+
 $("#add_user").submit(function(event){
     alert("Data Inserted Successfully !");
+})
+
+$("#update_user").submit(function(event){
+    event.preventDefault();
+
+    let unindexed_array = $(this).serializeArray();
+    let data = {}
+    $.map(unindexed_array,function(n,i){
+       data[n['name']]=n['value']; 
+    })
+
+    console.log(unindexed_array);
+
+    let request = {
+        "url": `http://localhost:3000/api/users/${data.id}`,
+        "method":"PUT",
+        "data":data
+    }
+    $.ajax(request).done(function(response){
+        alert("Data Updated successfully")
+    })
 })
