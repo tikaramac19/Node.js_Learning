7338bd82ffb725cf059fad81e8b9a485e8ba23e7
tikaramac19 <tikaramac19@gmail.com>
2022-07-26 15:31:50 +0545

    getting api from the backend using axios

---
+++ connectBackAndFrontEnd/client/src/service/api.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const URL = "http://localhost:8000";
+
+export const addUser = async (data) => {
+  try {
+    return await axios.post(`${URL}/add-user`, data);
+  } catch (error) {
+    console.log("Error while calling add User Api", error);
+  }
+};
