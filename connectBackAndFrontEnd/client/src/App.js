108e5f8714e8c3f37ef05a9cbcd4915f38eaa1c3
tikaramac19 <tikaramac19@gmail.com>
2022-07-26 14:31:58 +0545

    setting router for specific path

--- connectBackAndFrontEnd/client/src/App.js
+++ connectBackAndFrontEnd/client/src/App.js
@@ -1,11 +1,22 @@
-import './App.css';
-
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AddUser from "./components/AddUser";
+import AllUsers from "./components/AllUsers";
+import CrudApp from "./components/CrudApp";
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <div className="App">
-     <h2>Hello world !</h2>
+      <BrowserRouter>
+        <Navbar />
+        
+        <Routes>
+          <Route path="/" index element={<CrudApp />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/add-user" element={<AddUser />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-
   );
 }
 
