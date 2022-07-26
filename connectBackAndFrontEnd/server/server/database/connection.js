dcd3059c1fffbd539fc7fa6d9c2e0b9c0b1cd76d
tikaramac19 <tikaramac19@gmail.com>
2022-07-26 16:07:59 +0545

    database connected to the app

---
+++ connectBackAndFrontEnd/server/server/database/connection.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const connectDb = async()=>{
+    try{
+
+        const con = await mongoose.connect(process.env.MONGO_URL);
+
+        console.log(`MongoDB connected: ${con.connection.host}`);
+
+    }catch(err){
+        console.log(err);
+
+        process.exit(1);
+    }
+}
+
+module.exports = connectDb;
