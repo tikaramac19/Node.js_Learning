7f434056b57f35d6246468ff49c8640c916dc0d5
tikaramac19 <tikaramac19@gmail.com>
2022-07-26 15:03:07 +0545

    getting value from the input field

--- connectBackAndFrontEnd/client/src/components/AddUser.jsx
+++ connectBackAndFrontEnd/client/src/components/AddUser.jsx
@@ -1,9 +1,53 @@
 import React from "react";
+import FormGroup from "@mui/material/FormGroup";
+import {
+  FormControl,
+  Input,
+  InputLabel,
+  Typography,
+  styled,
+  Button
+} from "@mui/material";
+// import InputLabel from "@mui/material";
+
+const Container = styled(FormGroup)`
+  width: 50%;
+  margin: 1rem auto;
+
+  & > div{
+    margin-top: 1.3rem;
+  }
+`;
+
+const onValueChange = (e)=>{
+    console.log(e.target.value);
+}
 
 const AddUser = () => {
   return (
     <>
-      <h1>Add user page</h1>
+      <Container>
+        <Typography variant="h5"> Add User </Typography>
+        <FormControl>
+          <InputLabel>Name</InputLabel>
+          <Input onChange={(e)=> onValueChange(e)}/>
+        </FormControl>
+        <FormControl>
+          <InputLabel>Username</InputLabel>
+          <Input onChange={(e)=> onValueChange(e)}/>
+        </FormControl>
+        <FormControl>
+          <InputLabel>Email</InputLabel>
+          <Input onChange={(e)=> onValueChange(e)}/>
+        </FormControl>
+        <FormControl>
+          <InputLabel>Phone</InputLabel>
+          <Input onChange={(e)=> onValueChange(e)}/>
+        </FormControl>
+        <FormControl>
+        <Button variant="contained">Add User</Button>
+        </FormControl>
+      </Container>
     </>
   );
 };
