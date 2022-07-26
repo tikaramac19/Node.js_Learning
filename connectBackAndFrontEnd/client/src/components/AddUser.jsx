8619aede24342b7a750c17b7b8e0151b6827fdfa
tikaramac19 <tikaramac19@gmail.com>
2022-07-26 14:32:47 +0545

    specific comp created for specific reason

---
+++ connectBackAndFrontEnd/client/src/components/AddUser.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddUser = () => {
+  return (
+    <>
+      <h1>Add user page</h1>
+    </>
+  );
+};
+
+export default AddUser;
---
+++ connectBackAndFrontEnd/client/src/components/AllUsers.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AllUsers = () => {
+  return (
+   <>
+    <h1>AllUsers</h1>
+   </>
+  )
+}
+
+export default AllUsers
