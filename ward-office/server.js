a2eeb10f703539593497b8c9512f2a4e1742e108
tikaramac19 <tikaramac19@gmail.com>
2022-08-07 19:11:10 +0545

    load assets

--- ward-office/server.js
+++ ward-office/server.js
@@ -1,16 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
+const router = require("./server/routes/routes");
+const path = require("path");
 
-dotenv.config({path: ".env"});
+dotenv.config({ path: ".env" });
 const PORT = process.env.PORT || 8080;
 
-app.use("/", (req,res)=>{
-    res.send("server started !");
-})
+// set view engine
+app.set("view engine", "ejs"); // allows us to use ejs template engine
+
+// load assets
+
+app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
+
+app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
+
+app.use("/img", express.static(path.resolve(__dirname, "assets/img")));
 
 
+// loading routes
+app.use("/", router);
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running at http://localhost:${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
