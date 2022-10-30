f5f0dcb2ad16e0c277e33f809d819cbabd41f396
tikaramac19 <tikaramac19@gmail.com>
2022-10-30 16:41:01 +0545

    .

--- ward-office/passport-config.js
+++ ward-office/passport-config.js
@@ -7,14 +7,14 @@ const LocalStrategy = require("passport-local").Strategy;
     const user = getUserByEmail(email);
 
     if (user == null) {
-      return done(null, false, { message: "No user with that email !" });
+      return done(null, false, { message: "***  No user with that email !" });
     }
 
     try {
       if (await bcrypt.compare(password, user.password)) {
         return done(null, user);
       } else {
-        return done(null, false, { message: "Password incorrect" });
+        return done(null, false, { message: "***  Password incorrect" });
       }
     } catch (e) {
       return done(e);
