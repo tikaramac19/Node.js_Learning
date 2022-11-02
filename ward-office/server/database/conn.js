6d4182fbf2dec4048a4233622ef10c615e845524
tikaramac19 <tikaramac19@gmail.com>
2022-11-02 08:23:13 +0545

    connected to the database

--- ward-office/server/database/conn.js
+++ ward-office/server/database/conn.js
@@ -1,3 +1,22 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect()
+// database connection
+
+const connectDB = async()=>{
+    try{
+        // mongo db conncetion string
+        const con = await mongoose.connect(process.env.MONGO_URI, {
+            // not allows to print unwanted console warnings
+            // useNewUrlParser : true,
+            // useUnifiedTopology: true,
+            // useCreateIndex: true
+        }) 
+        console.log(`MongoDB connected: ${con.connection.host}`);
+    }catch(err){
+
+        console.log(err);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
