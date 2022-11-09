f0ddf11f257196765450b19d7755b8bb59536e72
tikaramac19 <tikaramac19@gmail.com>
2022-11-09 17:53:48 +0545

    get and post api done

--- ward-office/server/routes/routes.js
+++ ward-office/server/routes/routes.js
@@ -2,21 +2,30 @@ const express = require("express");
 const app = express();
 const router = express.Router();
 const services = require("../services/render");
+const BirthController = require("../controller/BirthController");
 
 router.get("/",checkAuthenticated, services.homeRoutes);
 
 // Root Router
 // method GET/birth-certificate
 
-router.get("/login",checkNotAuthenticated ,services.loginRoutes);
+router.get("/login",services.loginRoutes);
 
-router.get("/register",checkNotAuthenticated, services.signupRoutes);
+router.get("/register", services.signupRoutes);
 
 router.get("/birth-certificate",services.birthRoute);
-router.get("/death-certificate", checkAuthenticated,services.deathRoute)
-//API
+router.get("/birth-certificate/save",services.birthSave);
+router.get("/death-certificate", checkAuthenticated,services.deathRoute);
 
+//API Birth certificate
 
+// router.post('/api/birth/create', (req,res)=>{
+//     res.send(req.body);
+// });
+
+router.post('/api/birth/create', BirthController.create);
+// router.post('/api/birth/create', BirthController.createBirth);
+router.get('/api/birth/data', BirthController.find);
 
 function checkAuthenticated(req,res, next){  // if user doesnt logged in it redirect to the login page
     if(req.isAuthenticated()){
