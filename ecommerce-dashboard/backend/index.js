381f831303f193989d2171e432e267cc40f49c64
tikaramac19 <tikaramac19@gmail.com>
2022-11-19 08:56:19 +0545

    signin api ready

--- ecommerce-dashboard/backend/index.js
+++ ecommerce-dashboard/backend/index.js
@@ -1,16 +1,16 @@
 if (process.env.NODE_ENV !== "production") {
-    require("dotenv").config();
-  }
+  require("dotenv").config();
+}
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const connectDB = require('./server/Database/databaseConnect');
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const connectDB = require("./server/Database/databaseConnect");
 
 // signup model imports
-const SignUp = require('./server/Models/SignupModel');
+const SignUp = require("./server/Models/SignupModel");
 
 const PORT = process.env.PORT;
 
@@ -21,20 +21,33 @@ dotenv.config({ path: ".env" });
 // connect to the database
 connectDB();
 
-app.get('/', (req,res)=>{
-    res.send("welcome to ecommerce project");
-})
+app.get("/", (req, res) => {
+  res.send("welcome to ecommerce project");
+});
 
 // Signup api
-app.post("/register", async(req,res)=>{
+app.post("/register", async (req, res) => {
+  let user = new SignUp(req.body);
 
-    let user = new SignUp(req.body);
+  let result = await user.save();
 
-    let result = await user.save();
+  res.send(result);
+});
 
-    res.send(result);
-})
+// signIn api
+app.post("/login", async (req, res) => {
+  console.log(req.body);
 
-app.listen(PORT, (req,res)=>{
-    console.log(`Server run at : http://localhost:${PORT}`);
-})
+  if (req.body.password && req.body.email) {
+    let user = await SignUp.findOne(req.body).select("-password");
+
+    user ? res.send(user) : res.send({ result: "No user found" });
+    
+  } else {
+    res.send({ result: "NO user found." });
+  }
+});
+
+app.listen(PORT, (req, res) => {
+  console.log(`Server run at : http://localhost:${PORT}`);
+});
