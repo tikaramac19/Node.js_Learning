6a244ba374577ed821b83250ed0db96763bf7b17
tikaramac19 <tikaramac19@gmail.com>
2022-11-20 16:36:16 +0545

    login route created

--- ecommerce-dashboard/frontend/src/App.js
+++ ecommerce-dashboard/frontend/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import Signup from "./components/SignUp/Signup";
 import PrivateComponent from "./components/PrivateComp/PrivateComponent";
-
+import Login from "./components/Login/Login";
 function App() {
   return (
     <div className="App">
@@ -15,10 +15,10 @@ function App() {
             <Route path="/" element={<h2> Product Listing </h2>} exact />
             <Route path="/add" element={<h2> Add Product </h2>} />
             <Route path="/update" element={<h2> Update Product </h2>} />
-            {/* <Route path="/logout" element={<h2> Logout dashboard </h2>} /> */}
             <Route path="/profile" element={<h2> Profile section </h2>} />
           </Route>
 
+          <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
         </Routes>
         <Footer />
