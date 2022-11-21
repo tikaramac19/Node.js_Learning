f26033cafb7e6b31055f227600d0d48a151de3d0
tikaramac19 <tikaramac19@gmail.com>
2022-11-21 09:38:49 +0545

    navbar updated

--- ecommerce-dashboard/frontend/src/components/Navbar/Navbar.js
+++ ecommerce-dashboard/frontend/src/components/Navbar/Navbar.js
@@ -1,55 +1,65 @@
 import React, { useEffect } from "react";
 import styles from "./Navbar.module.css";
 
-import { Link, useNavigate } from "react-router-dom";
+import { json, Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const auth = localStorage.getItem("user");
+
+  // console.log(auth);
+
   const navigate = useNavigate();
   // logout function
   const logOut = () => {
     localStorage.clear(); // it will clear the localstorage
-    navigate("/login");
+    navigate("/signin");
   };
 
   return (
     <>
       <div className={styles.navbar_container}>
-        <ul>
-          <li>
-            <Link to="/">Products</Link>
-          </li>
-          <li>
-            <Link to="/add"> Add Products</Link>
-          </li>
-          <li>
-            <Link to="/update">Update Products</Link>
-          </li>
+        <img
+          src="https://www.jetpunk.com/img/user-photo-library/3a/3a1b007d7b-450.png"
+          alt="logo"
+        />
 
-          <li>
-            {auth ? (
-              <>
-                <li>
-                  <Link to="/profile">Profile</Link>{" "}
-                </li>
-                <li>
-                  <Link onClick={logOut} to="/signup">
-                    Logout
-                  </Link>
-                </li>
-              </>
-            ) : (
-              <>
-                <li>
-                  <Link to="/signup">Signup</Link>
-                </li>
-                <li>
-                  <Link to="/login">Login</Link>
-                </li>
-              </>
-            )}
-          </li>
-        </ul>
+        {auth ? (
+          <>
+            <ul className={styles.product_cont}>
+              <li>
+                <Link to="/">Products</Link>
+              </li>
+              <li>
+                <Link to="/add"> Add Products</Link>
+              </li>
+              <li>
+                <Link to="/update">Update Products</Link>
+              </li>
+              <li>
+                <Link to="/profile">Profile</Link>{" "}
+              </li>
+              <li>
+                <Link onClick={logOut} to="/signin">
+                  Logout
+                  <span className={styles.userName}>
+                    ({JSON.parse(auth).name})
+                  </span>
+                </Link>
+              </li>
+            </ul>
+          </>
+        ) : (
+          <>
+            <ul className={styles.auth_container}>
+              <li>
+                <Link to="/signup">Signup</Link>
+              </li>
+              <li>
+                <Link to="/signin">Login</Link>
+              </li>
+            </ul>
+          </>
+        )}
       </div>
     </>
   );
